60d14f9235d4a621d43c0a4db2369c77c304ab18
Siddharth Garewal <sidgarewal@gmail.com>
2021-07-31 17:31:58 +0530

    login functionality added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm'
 import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine
             height="100vh"
             projectID="d5946b8c-f6c9-42e1-8681-7a5df4a305ba"
-            userName="Siddharth"
-            userSecret="12345"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
